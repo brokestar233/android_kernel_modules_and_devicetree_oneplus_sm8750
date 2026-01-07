1b17742f7c60ca7dd032fc3b85ad4245b9c0d2e2
brokestar233 <3765589194@qq.com>
2026-01-07 17:27:45 +0800

    qcacld3: Add monitor mode support to netlink
    Link:https://github.com/kimocoder/android_kernel_oneplus_avicii/commit/a8ac9d836544cbaa4e1a58a9cfc0f1e3548dcb9e
    Signed-off-by: brokestar233 <3765589194@qq.com>

--- vendor/qcom/opensource/wlan/qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211.c
+++ vendor/qcom/opensource/wlan/qcacld-3.0/core/hdd/src/wlan_hdd_cfg80211.c
@@ -26337,6 +26337,7 @@ static bool hdd_is_client_mode(enum QDF_OPMODE mode)
 	case QDF_STA_MODE:
 	case QDF_P2P_CLIENT_MODE:
 	case QDF_P2P_DEVICE_MODE:
+	case QDF_MONITOR_MODE:
 		return true;
 	default:
 		return false;
